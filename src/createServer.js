af628de815aaeb4e09d36567b8383ddf5efe5d91
ab3MN <ab3mn3@gmail.com>
2024-12-05 22:46:42 +0200

    refactor:fix all The AI comments

--- src/createServer.js
+++ src/createServer.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
+const { serverError } = require('./errors');
 
 function createServer() {
   const server = new http.Server();
@@ -11,34 +12,40 @@ function createServer() {
     const url = new URL(req.url, `http://${req.headers.host}`);
 
     if (req.method === 'POST' && url.pathname === '/add-expense') {
-      const chunks = [];
+      try {
+        const chunks = [];
 
-      for await (const chunk of req) {
-        chunks.push(chunk);
-      }
-
-      const text = Buffer.concat(chunks).toString();
-      const data = JSON.parse(text);
-      const { date, title, amount } = data;
-
-      if (!date || !title || !amount) {
-        res.statusCode = 400;
-
-        return res.end('All the field must be field');
-      }
-
-      fs.writeFile(path.join(__dirname, '../db/expense.json'), text, (err) => {
-        if (err) {
-          res.statusCode = 500;
-
-          return res.end('Internal server Error');
+        for await (const chunk of req) {
+          chunks.push(chunk);
         }
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        const text = Buffer.concat(chunks).toString();
+        const data = JSON.parse(text);
+        const { date, title, amount } = data;
 
-        return res.end(text);
-      });
+        if (!date || !title || !amount) {
+          res.statusCode = 400;
+
+          return res.end('All fields must be filled');
+        }
+
+        fs.writeFile(
+          path.join(__dirname, '../db/expense.json'),
+          text,
+          (err) => {
+            if (err) {
+              return serverError(res, err);
+            }
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+
+            return res.end(text);
+          },
+        );
+      } catch (err) {
+        return serverError(res, err);
+      }
     } else {
       res.statusCode = 404;
       res.end('Not Found');
