2967e2707346570722db0140be9912ee553286d7
ab3MN <ab3mn3@gmail.com>
2024-12-05 22:34:23 +0200

    feat:implemented solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,51 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = new http.Server();
+
+  server.on('request', async (req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    if (req.method === 'POST' && url.pathname === '/add-expense') {
+      const chunks = [];
+
+      for await (const chunk of req) {
+        chunks.push(chunk);
+      }
+
+      const text = Buffer.concat(chunks).toString();
+      const data = JSON.parse(text);
+      const { date, title, amount } = data;
+
+      if (!date || !title || !amount) {
+        res.statusCode = 400;
+
+        return res.end('All the field must be field');
+      }
+
+      fs.writeFile(path.join(__dirname, '../db/expense.json'), text, (err) => {
+        if (err) {
+          res.statusCode = 500;
+
+          return res.end('Internal server Error');
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+
+        return res.end(text);
+      });
+    } else {
+      res.statusCode = 404;
+      res.end('Not Found');
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
